229816397da120c4f271054562e2984f29b272ee
Dom Goodwin <dom.goodwin@capgemini.com>
2017-12-04 13:02:03 +0000

    Day 4 refactor

---
+++ day4/day4/src/main/scala/Main.scala
@@ -0,0 +1,15 @@
+import scala.io.Source
+
+object Main extends App {
+  println("Hello, World dom!")
+  val filename = "day4.txt";
+
+  val validPhrases1 = Source.fromFile(filename).getLines.map(l => if(l.split(" ").toSet.size == l.split(" ").size){ l } else { null});
+  val count1 = validPhrases1.count(x => x != null);
+  println(s"[PART1] Count of valid phrases: $count1");
+
+  val validPhrases2 = Source.fromFile(filename).getLines.map(l => if(l.split(" ").map(p => p.sorted) ==  )
+
+
+
+}
