04625972f393890c68b4028c20857759fdd4853e
Dom Goodwin <dom.goodwin@capgemini.com>
2017-12-04 13:04:41 +0000

    Day 4 part 2

--- day4/day4/src/main/scala/Main.scala
+++ day4/day4/src/main/scala/Main.scala
@@ -8,8 +8,10 @@ object Main extends App {
   val count1 = validPhrases1.count(x => x != null);
   println(s"[PART1] Count of valid phrases: $count1");
 
-  val validPhrases2 = Source.fromFile(filename).getLines.map(l => if(l.split(" ").map(p => p.sorted) ==  )
-
+  val validPhrases2 = Source.fromFile(filename).getLines
+    .map(l => if(l.split(" ").map(p => p.sorted).toSet.size == l.split(" ").map(p => p.sorted).size) { l } else { null} );
+  val count2 = validPhrases2.count(x => x != null);
+  println(s"[PART2] Count of valid phrases: $count2");
 
 
 }
