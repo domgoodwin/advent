13887f7282ae7d020cf08d224ee55f0864170754
Goodwin, Dom <dom.goodwin@capgemini.com>
2018-04-24 15:13:27 +0100

    Part 2

--- day15.py
+++ day15.py
@@ -18,11 +18,31 @@ gen_b = 516
 loop = 40000000
 matches = 0
 
-for i in range(0, loop):
+#part2
+gen_a_vals = []
+gen_b_vals = []
+loop = 5000000
+
+
+while(len(gen_a_vals) < loop or len(gen_b_vals) < loop):
     gen_a = (gen_a * 16807) % 2147483647
-    gen_b = gen_b * 48271 % 2147483647
-    if(convertAndCompare(gen_a, gen_b)):
+    gen_b = (gen_b * 48271) % 2147483647
+    if(gen_a % 4 == 0):
+        gen_a_vals.append(gen_a)
+    if(gen_b % 8 == 0):
+        gen_b_vals.append(gen_b)
+    # if(convertAndCompare(gen_a, gen_b)):
+    #     matches += 1
+
+# TODO make work for part 1 too
+# part 2
+matches = 0
+print(len(gen_a_vals), len(gen_b_vals))
+for i in range(0, loop - 1):
+    if(convertAndCompare(gen_a_vals[i], gen_b_vals[i])):
         matches += 1
+
+#generator A uses 65, while generator B uses 8921 // of 309 pairs
 
 print(matches)
 
