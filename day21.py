8ef21db62a7496a62ce9afbfe88cb4d45cea3df2
Goodwin, Dom <dom.goodwin@capgemini.com>
2018-05-01 15:31:48 +0100

    Day 2

--- day21.py
+++ day21.py
@@ -1,19 +1,8 @@
 import numpy as np
 import math
 
-f = open('inputs/day21.txt', 'r')
-
-en_rules = {}
-
-for line in f:
-     rule = line.split(" => ")
-     en_rules[rule[0]] = rule[1].strip()
-
-#print(en_rules)
-
-
 def getSingleUnit(grid):
-    #print(grid)
+    ##print(grid)
     output = ""
     for row in grid:
         for col in row:
@@ -44,10 +33,8 @@ def getAlts(grid):
     for i in range(0, 4):
         np_arr = np.rot90(np_arr)
         alts.append(np_arr.tolist())
-    for i in range(0, 4):
         fl0 = np.rot90(fl0)
         alts.append(fl0.tolist())
-    for i in range(0, 4):
         fl1 = np.rot90(fl1)
         alts.append(fl1.tolist())
     return [getSingleUnit(a) for a in alts]
@@ -62,36 +49,28 @@ def getCount(f_pat):
         count += len(n)
     return count
 
-#   Starting pattern
-#   .#.
-#   ..#
-#   ###
+f = open('inputs/day21.txt', 'r')
+
+## Process input as rules
+en_rules = {}
+for line in f:
+     rule = line.split(" => ")
+     en_rules[rule[0]] = rule[1].strip()
 
 pat =  [['.', '#', '.'],
         ['.', '.', '#'],
         ['#', '#', '#']]
 
-# pat =  [['.', '#', '.','.', '.', '#'],
-#         ['.', '.', '#','#', '#', '.'],
-#         ['#', '#', '#','.', '#', '.'],
-#         ['#', '#', '.','#', '.', '#'],
-#         ['.', '.', '#','#', '#', '.'],
-#         ['#', '#', '#','.', '.', '#']]
-
-
 grids = []
 
-
-#print(grids)
-print(en_rules)
 count = 0
-
-while(count < 5):
-    print("before", pat)
+while(count < 18):
+    print("iteration:",count)
     g_size = 0
     size = len(pat)
     grids = []
-    # assumption: list len is always size
+
+    ## Split into smaller grids
     if(len(pat) % 2 == 0):
         g_size = 2
         for y in range(0, len(pat), 2):
@@ -109,63 +88,55 @@ while(count < 5):
                     [pat[y+1][x],pat[y+1][x+1],pat[y+1][x+2]], 
                     [pat[y+2][x],pat[y+2][x+1],pat[y+2][x+2]]
                     ])
-    #print(grids)
+
+    ## Flatten grids and compare with rule dictionary
     n_flat_grids = []
     for grid in grids:
         alts = getAlts(grid)
         change = ""
         for alt in alts:
-            print(alt)
             if(alt in en_rules):
                 change = alt
-                print(alt, change, "!!")
                 break
         if(change != ""):
-            print("making a change", change)
             n_flat_grids.append(en_rules[change].strip())
         else:
             n_flat_grids.append(getSingleUnit(grid).strip())
         change = ""
-    print(n_flat_grids)
-    # pair back together list of new flats into new pattern
-    i = 0
-    rg = 0
+
+    ## Convert new flat grids into pattern
     n_pat = []
-    # find out size of whole new grid
-    
+
+    # Create empty grid
     t_count = getCount(n_flat_grids)
     g_len = int(math.sqrt(t_count))
+    i = 0
     for i in range(0, g_len): # col count
         n_pat.append([])
-    #print(n_pat)
+
     row = 0 #  0 is top row, 1 is down one
     col = 0 # 0 is leftmost
     for grid in n_flat_grids:
         
         items = grid.strip().split("/")
+        # Check if row is full
         if(col >= g_len):
             col = 0
             row += len(items[0])
-
         i = 0
+        # Add items to rows
         for item in items:
-            #print(row, col, i)
             for ch in item:
                 n_pat[row + i].append(ch)
             i += 1
         col += len(items[0])
     
-    print("after",n_pat)
     pat = n_pat
-    # list will be top row, LEFT TO RIGHT, then next row (block)
     count += 1
 
 on_count = 0
+# Calculate and count hashes
 for row in pat:
     for ch in row:
         on_count += 1 if ch == "#" else 0
 print("part1", on_count)
-
-    
-
-
