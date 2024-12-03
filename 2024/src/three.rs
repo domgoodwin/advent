84098e7fba8f0f904c71f09861643451d63005e4
Dom Goodwin <git@dgood.win>
2024-12-03 17:52:37 +0000

    aoc2024 day 3 part 1

--- 2024/src/three.rs
+++ 2024/src/three.rs
@@ -3,11 +3,21 @@ use regex::Regex;
 
 pub fn day_three() {
     let data = fs::read_to_string("./inputs/3.txt").expect("Unable to read file");
+    part_one(data);
+}
 
+
+fn part_one(data: String) {
     let re: Regex = Regex::new("mul\\([0-9]+,[0-9]+\\)").unwrap();
-    if let Some(mat) = re.find(data) {
-        println!("{}", mat.as_str())
+    let mut total = 0;
+     for mat in re.find_iter(&data) {
+        let to_split = mat.as_str().replace("mul(", "");
+        let to_split = to_split.replace(")", "");
+        let parts: Vec<&str> = to_split.split(",").collect();
+        let left =  parts[0].parse::<i32>().unwrap();
+        let right =  parts[1].parse::<i32>().unwrap();
+        
+        total += left * right;
      }
-
-    
+     println!("part one: {}", total);
 }
