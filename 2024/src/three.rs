a96dd2d13af12c95f0b25a97b5f8fc85d3525b30
Dom Goodwin <git@dgood.win>
2024-12-03 17:58:37 +0000

    aoc2024 day 3 part 2

--- 2024/src/three.rs
+++ 2024/src/three.rs
@@ -3,7 +3,8 @@ use regex::Regex;
 
 pub fn day_three() {
     let data = fs::read_to_string("./inputs/3.txt").expect("Unable to read file");
-    part_one(data);
+    part_one(data.clone());
+    part_two(data);
 }
 
 
@@ -20,4 +21,31 @@ fn part_one(data: String) {
         total += left * right;
      }
      println!("part one: {}", total);
+}
+
+
+fn part_two(data: String) {
+    let re: Regex = Regex::new("(mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\))").unwrap();
+    let mut total = 0;
+    let mut enabled = true;
+     for mat in re.find_iter(&data) {
+        if mat.as_str().contains("mul") {
+            if !enabled{
+                continue;
+            }
+            let to_split = mat.as_str().replace("mul(", "");
+            let to_split = to_split.replace(")", "");
+            let parts: Vec<&str> = to_split.split(",").collect();
+            let left =  parts[0].parse::<i32>().unwrap();
+            let right =  parts[1].parse::<i32>().unwrap();
+            total += left * right;
+        }
+        if mat.as_str().contains("don") {
+            enabled = false;
+        } else if mat.as_str().contains("do") {
+            enabled = true;
+        }
+
+     }
+     println!("part two: {}", total);
 }
